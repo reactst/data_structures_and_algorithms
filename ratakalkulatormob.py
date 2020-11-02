020f32c0bfe016e5f72269a8581f64e002aed9a4
reactst <reactst@gmail.com>
2020-11-02 15:41:14 +0100

    Phone plan calculator v0.3
    Working flawlessly 4 now

---
+++ ratakalkulatormob.py
@@ -0,0 +1,4 @@
+pp=int(input("Koliko je rata uređaja? "))
+op=int(input("Koliko plaćate odmah? "))
+brm=int(input("Na koliko mjeseci? "))
+print ("Kroz",brm,"mjeseca/i mobitel će te ukupno platiti:",pp*brm+op)
