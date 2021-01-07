e41c2f510179d7ad8ec5cc333215376dd0e09620
reactst <reactst@gmail.com>
2021-01-07 13:21:18 +0100

    notyetwtf

--- 5.vj+obrana/dictionary.c
+++ 5.vj+obrana/dictionary.c
@@ -260,17 +260,17 @@ void add_second_third (Dictionary dict, char* str1, char* str2){
 void longestword_firstplace (Dictionary dict){
     Dictionary dummy=dict;
     Dictionary prvi=dict->next;
-    Dictionary prijedict=dict;
+    Dictionary pomocni=dict;
     dict = dict->next;
     Dictionary spremijosjedan=dict->next;
     Dictionary temp = dict;
-    while (dict != NULL){
+    dict=dict->next;
+    while (dict->next != NULL){
         if (strlen(dict->word) > strlen(temp->word)){
             temp->word = dict->word;
-            prijedict->next=prijedict->next;
         }
-        prijedict=prijedict->next;
         dict = dict->next;
+        pomocni=pomocni->next;
     }
     dummy->next=temp;
     temp->next=prvi;
