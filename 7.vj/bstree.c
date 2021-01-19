405361ffc319828940ecca0128912be946193b1a
reactst <reactst@gmail.com>
2021-01-19 20:05:18 +0100

    ZAPOCETO

---
+++ 7.vj/bstree.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <malloc.h>
+#include "bstree.h"
+
+BSTree NewBSTree()
+{
+	// Novo prazno stablo
+	return NULL;
+}
+
+void AddNode(BSTree *bst, char *word)
+{
+	// Rekurzivno se traži mjesto za novi èvor u stablu. Ako rijeè postoji u stablu, ne dodaje se.
+	// bst parametar je dvostruki pokazivaè.
+
+}
+
+int BSTHeight(BSTree bst)
+{
+	// Rekurzivno se prolazi cijelo stablo da bi se pronašla najduža grana (visina stabla).
+	
+	return 0;
+}
+
+void PrintBSTree(BSTree bst)
+{
+	// Ispisuje rijeèi u stablu na ekran po abecednom redu.
+	// In-order šetnja po stablu (lijevo dijete, èvor, desno dijete)
+
+}
+
+void SaveBSTree(BSTree bst, FILE *fd)
+{
+	// Snima rijeè po rijeè iz stabla u tekstualnu datoteku. Rijeèi su odvojene razmakom.
+	// Pre-order šetnja po stablu (ttenutni èvor pa djeca)
+
+}
+
+void DeleteBSTree(BSTree bst)
+{
+	// Briše stablo (string word i sam èvor) iz memorije.
+	// Post-order šetnja po stablu (prvo djeca pa trenutni èvor)
+
+}
+
+BSTree LoadBSTree(FILE *fd)
+{
+	// Uèitava rijeè po rijeè iz tekstualne datoteke i dodaje ih u stablo preko AddNode() funkcije.
+	// Rijeè duplicirati sa strdup().
+
+	return NULL;
+}
---
+++ 7.vj/readme.md
@@ -0,0 +1,14 @@
+7. Laboratorijska vježba
+
+
+Binarna stabla
+
+Implementacija rječnika sa binarnim stablom pretrage
+
+Implementirati funkcije u kôdu za binarno stablo pretrage (opis u kôdu). Biti će potrebno implementirati različite šetnje po stablu:
+
+    • Postorder (prvo djeca pa zatim trenutni čvor) - za brisanje stabla, da se ne izgube pokazivači na djecu.
+    • Preorder (prvo trenutni čvor pa zatim djeca) -  za snimanje stabla istim redoslijedom kao što su čvorovi dodani u stablo.
+    • Inorder (prvo lijevo dijete, pa trenutni čvor, i desno dijete) – za ispis riječi po abecednom redoslijedu.
+
+
